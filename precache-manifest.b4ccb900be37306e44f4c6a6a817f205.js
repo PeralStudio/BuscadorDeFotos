300fe1941d42050a84f12cd01349c173afc4bf97
PeralStudio <info@peralstudio.com>
2020-06-10 18:31:16 +0200

    Updates

---
+++ precache-manifest.b4ccb900be37306e44f4c6a6a817f205.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7662a295f59b566717a4412b4ca52150",
+    "url": "/BuscadorDeFotos/index.html"
+  },
+  {
+    "revision": "c961bdea68c07082ae7d",
+    "url": "/BuscadorDeFotos/static/js/2.9dee5255.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/BuscadorDeFotos/static/js/2.9dee5255.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a8176bdd2580fe94590",
+    "url": "/BuscadorDeFotos/static/js/main.07c3cd36.chunk.js"
+  },
+  {
+    "revision": "c8ad79eab23418610646",
+    "url": "/BuscadorDeFotos/static/js/runtime-main.04d830a3.js"
+  }
+]);
